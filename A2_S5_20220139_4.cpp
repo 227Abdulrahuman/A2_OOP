a3e5e64a8d17183787ca5c9271fc67cb1c09c6ff
remo <remo@test.com>
2023-10-30 12:27:06 +0200

    uploading problem

--- A2_S5_20220139_4.cpp
+++ A2_S5_20220139_4.cpp
@@ -1,31 +1,51 @@
-﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 17
-VisualStudioVersion = 17.4.33213.308
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "Assignment_2_Q_4", "Assignment_2_Q_4\Assignment_2_Q_4.vcxproj", "{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|x64 = Debug|x64
-		Debug|x86 = Debug|x86
-		Release|x64 = Release|x64
-		Release|x86 = Release|x86
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Debug|x64.ActiveCfg = Debug|x64
-		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Debug|x64.Build.0 = Debug|x64
-		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Debug|x86.ActiveCfg = Debug|Win32
-		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Debug|x86.Build.0 = Debug|Win32
-		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Release|x64.ActiveCfg = Release|x64
-		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Release|x64.Build.0 = Release|x64
-		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Release|x86.ActiveCfg = Release|Win32
-		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Release|x86.Build.0 = Release|Win32
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {C605FAFB-90E4-4E10-850C-6D13C1932087}
-	EndGlobalSection
-EndGlobal
+//File: A2_S5_20221099_4.cpp
+//purpose: print prime numbers from 2 to N
+//Author: Remon raafat nassif
+//Section: S5
+//ID: 20220139
+//TA: Eng/Maya
+//Date: 30 Oct 2023
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void  thesieveofEratosthenes();
+
+int main()
+{
+    thesieveofEratosthenes();
+    return 0;
+}
+
+void  thesieveofEratosthenes()
+{
+    int n;
+    cout << "Enter the maximum number : ";
+    cin >> n;
+    vector<int> prime;
+    for (int i = 2; i <= n; i++)
+    {
+        prime.push_back(i);
+    }
+    for (auto k : prime)
+        cout << k << " ";
+    cout << "\n************************************\n";
+    for (int i = 2; i * i <= n; i++)
+    {
+        for (int j = 0; j < prime.size(); j++)
+        {
+            if (prime[j] % i == 0 && prime[j] != i)
+            {
+                prime[j] = 0;
+            }
+        }
+        for (auto k : prime)
+        {
+            if (k != 0) cout << k << " ";
+        }
+        cout << "\n************************************\n";
+
+    }
+
+}
