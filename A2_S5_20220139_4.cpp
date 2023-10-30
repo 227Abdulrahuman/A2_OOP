879f6e3da6a07cfa0a263ad463a9be2c61c14851
remo <remo@test.com>
2023-10-30 12:23:44 +0200

    uploading first problem

---
+++ A2_S5_20220139_4.cpp
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.4.33213.308
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "Assignment_2_Q_4", "Assignment_2_Q_4\Assignment_2_Q_4.vcxproj", "{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Debug|x64.ActiveCfg = Debug|x64
+		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Debug|x64.Build.0 = Debug|x64
+		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Debug|x86.ActiveCfg = Debug|Win32
+		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Debug|x86.Build.0 = Debug|Win32
+		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Release|x64.ActiveCfg = Release|x64
+		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Release|x64.Build.0 = Release|x64
+		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Release|x86.ActiveCfg = Release|Win32
+		{BFFA66EF-6E0A-4A1A-BE87-AA259829AF06}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {C605FAFB-90E4-4E10-850C-6D13C1932087}
+	EndGlobalSection
+EndGlobal
